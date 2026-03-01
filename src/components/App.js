93fa48c9a03f51a5c1882ab0757999ecd589f45e
dahirfarhia44 <dahirfarhia44@gmail.com>
2026-03-01 16:39:04 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,33 @@ import Header from "./Header";
 import itemData from "../data/items";
 
 function App() {
+  // Items state (so we can add new ones)
   const [items, setItems] = useState(itemData);
+
+  // Dark mode state
   const [isDarkMode, setIsDarkMode] = useState(false);
 
+  // Toggle dark mode
   function handleDarkModeClick() {
-    setIsDarkMode((isDarkMode) => !isDarkMode);
+    setIsDarkMode((prevMode) => !prevMode);
+  }
+
+  // Add new item to list
+  function handleAddItem(newItem) {
+    setItems((prevItems) => [...prevItems, newItem]);
   }
 
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <Header
+        isDarkMode={isDarkMode}
+        onDarkModeClick={handleDarkModeClick}
+      />
+
+      <ShoppingList
+        items={items}
+        onItemFormSubmit={handleAddItem}
+      />
     </div>
   );
 }
